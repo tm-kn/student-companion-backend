ded91f3872ecd6a1b2da50b8d87ef5a7599b7a98
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2016-10-25 15:29:47 +0100

    Change required fields for Place model

--- student_companion/places/admin.py
+++ student_companion/places/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Place, PlaceCategory, PlaceTag
+
+
+@admin.register(PlaceCategory)
+class PlaceCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(PlaceTag)
+class PlaceTagAdmin(admin.ModelAdmin):
+    pass
--- student_companion/places/models.py
+++ student_companion/places/models.py
@@ -5,10 +5,10 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class PlaceCategory(MPTTModel):
-    parent_category = TreeForeignKey('self', null=True, blank=True,
-                                     related_name='category_children',
-                                     db_index=True,
-                                     verbose_name=_('parent category'))
+    parent = TreeForeignKey('self', null=True, blank=True,
+                            related_name='category_children',
+                            db_index=True,
+                            verbose_name=_('parent category'))
     name = models.CharField(_('name'), max_length=40)
     slug = models.SlugField(_('slug'), max_length=20, unique=True)
 
@@ -35,27 +35,32 @@ class Place(models.Model):
         (VERY_EXPENSIVE, _('very expensive')),
     )
 
-    name = models.CharField(_('place'), max_length=30)
+    name = models.CharField(_('name'), max_length=30)
+    slug = models.SlugField(_('slug'), max_length=30, unique=True)
     is_visible = models.BooleanField(_('is visible'), default=False)
     google_places_id = models.CharField(_('Google API Place ID'),
                                         max_length=100, blank=True,
                                         unique=True)
     categories = models.ManyToManyField('PlaceCategory', related_name='place',
-                                        verbose_name=_('categories'))
-    description = models.TextField(_('description'))
+                                        verbose_name=_('categories'),
+                                        blank=True)
+    description = models.TextField(_('description'), blank=True)
     website = models.URLField(_('website'), blank=True)
-    address = models.CharField(_('formatted address'), max_length=255)
+    address = models.CharField(_('formatted address'), max_length=255,
+                               blank=True)
     telephone_number = models.CharField(_('formatted telephone number'),
                                         max_length=15, blank=True)
     facebook_handle = models.CharField(_('Facebook username'), max_length=255,
                                        blank=True)
     twitter_handle = models.CharField(_('Twitter username'), max_length=255,
                                       blank=True)
-    student_discount = models.CharField(_('student discount'), max_length=50)
+    student_discount = models.CharField(_('student discount'), max_length=50,
+                                        blank=True)
     opening_times = models.TextField(_('opening hours'), blank=True)
     price_level = models.PositiveSmallIntegerField(_('price level'),
-                                                   choices=PRICE_LEVEL_CHOICES)
-    tags = models.ManyToManyField('PlaceTag')
+                                                   choices=PRICE_LEVEL_CHOICES,
+                                                   blank=True, null=True)
+    tags = models.ManyToManyField('PlaceTag', blank=True)
 
     class Meta:
         verbose_name = _('place')

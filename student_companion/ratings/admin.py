4854c62601cc885ba372258ef9a28555a07dfb0b
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2016-12-06 15:04:33 +0000

    Add basic admin for ratings

--- student_companion/ratings/admin.py
+++ student_companion/ratings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PlaceRating
+
+
+@admin.register(PlaceRating)
+class PlaceRatingAdmin(admin.ModelAdmin):
+    pass
